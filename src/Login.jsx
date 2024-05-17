c6b00ca849a600292950c5718e5556ced2124a9e
ks-v-engg <kamakshisundaramv@gmail.com>
2024-05-17 21:07:27 +0530

    Login page changes

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Routetodo } from "./Route";
+import Form from 'react-bootstrap/Form';
+import { Button } from "react-bootstrap";
+import { Row, Col} from "react-bootstrap";
 
 const Login = () => {
 
@@ -9,13 +12,54 @@ const loginnavi =  useNavigate();
 const login = () => {
     loginnavi(Routetodo.todolist)
     }
-    
-return <div>
-        <h2>Login</h2>
-        <input type="text" placeholder="Username" id="username"/>
-        <input type="password" placeholder="Password" id="password"/>
-        <button onClick={login}>Login</button>
-</div>
-} 
+const [email,Setemail] = React.useState('');
+const [passowrd,Setpassword] = React.useState('');
 
-export default Login;
+const [result,setResult] = React.useState('');
+
+const onSubmit = (event) => {
+    event.preventDefault();
+   
+   if (email === 'test@gmail.com' && passowrd === '12345678') 
+    {
+        setResult('Login Sucessful');
+        login();
+    } else {
+        setResult('Login Unsucessful');
+    }
+    
+}
+
+return  <div> 
+<Row>
+    <Col class="text-center"> <h3>Login</h3></Col>
+</Row>
+
+<div class="d-flex justify-content-center align-items-center vh-100">
+
+    <Form onSubmit={onSubmit}>
+    <Form.Control
+        type="text" 
+        value={email} 
+        name="email" 
+        id="email" 
+        placeholder="Enter email" 
+        onChange={(e) => Setemail(e.target.value)}/> <br />
+    <Form.Control 
+        type="password" 
+        value={passowrd} 
+        name="password" 
+        id="password" 
+        placeholder="Enter password" 
+        onChange={(e) => Setpassword(e.target.value)}/> <br />
+    <Button variant="primary"
+        type="submit" 
+        value={"Login"}> Login </Button>
+    <br />
+    <h3>{result}</h3>
+    </Form>
+</div>
+</div>
+}
+
+export default Login
